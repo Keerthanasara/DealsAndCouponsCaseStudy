a61153304b230157ac4a504bfd8ea623ed80ddeb
Keerthanasara <115665836+Keerthanasara@users.noreply.github.com>
2022-12-15 15:06:51 +0530

    Add files via upload

---
+++ CartPayService/src/main/java/com/dealsandcouponsfinder/CartPayService/model/Cart.java
@@ -0,0 +1,75 @@
+package com.dealsandcouponsfinder.CartPayService.model;
+
+import java.util.List;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
+
+@Document(collection = "CartDetails")  //to map into a collection inside mongodb
+public class Cart {
+
+	@Id //used for identity purpose
+	private String cartId;
+	@Field
+	private String userId;
+	@Field
+	private int quantity;
+	@Field
+	private String couponId;
+	@Field
+	private double price;
+	
+	public String getCartId() {
+		return cartId;
+	}
+	public void setCartId(String cartId) {
+		this.cartId = cartId;
+	}
+	public String getUserId() {
+		return userId;
+	}
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+	public int getQuantity() {
+		return quantity;
+	}
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+	public String getCouponId() {
+		return couponId;
+	}
+	public void setCouponId(String couponId) {
+		this.couponId = couponId;
+	}
+	public double getPrice() {
+		return price;
+	}
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	
+	@Override
+	public String toString() {
+		return "Cart [cartId=" + cartId + ", userId=" + userId + ", quantity=" + quantity + ", couponId=" + couponId
+				+ ", price=" + price + "]";
+	}
+	
+	public Cart(String cartId, String userId, int quantity, String couponId, double price) {
+		super();
+		this.cartId = cartId;
+		this.userId = userId;
+		this.quantity = quantity;
+		this.couponId = couponId;
+		this.price = price;
+		
+	}
+
+	
+	public Cart() {
+		
+	}
+
+}
---
+++ CartPayService/src/test/java/com/dealsandcouponsfinder/CartPayService/CartPayServiceApplicationTests.java
@@ -0,0 +1,14 @@
+
+package com.dealsandcouponsfinder.CartPayService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CartPayServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
